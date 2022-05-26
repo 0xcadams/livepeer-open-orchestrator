7b5ef49208c93d47a74fe04f0bb1b343ce3ea26e
Chase Adams <c@cadams.io>
2022-05-25 21:19:12 -0700

    initial eks/helm (#1)
    * feat: 🎸 initial commit
    
    * feat: 🎸 added charts and aws roles
    
    * ci: 🎡 added linting to ci
    
    * ci: 🎡 fix helm ci version
    
    * fix: 🐛 fixed updates and adding hardened configs
    
    * fix: 🐛 further hardening of deploy scripts
    
    * feat: 🎸 added nvidia instances and deploy from ci
    
    * ci: 🎡 fix ci
    
    * fix: 🐛 dumb ignore of yarn lock

---
+++ prepare.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import regions from 'aws-regions';
+import mustache from 'mustache';
+
+const eksFileTemplate = 'eksctl.mustache.yaml';
+
+const main = async () => {
+  const awsRegion = process.env.AWS_REGION;
+  const projectName = process.env.PROJECT_NAME;
+
+  if (!awsRegion || !projectName) {
+    throw new Error('You must define all environment variables.');
+  }
+  if (!regions.lookup({ code: awsRegion })) {
+    throw new Error('The AWS_REGION you provided is not a valid value. Please try again.');
+  }
+
+  const source = await fs.readFileSync(eksFileTemplate, 'utf-8');
+
+  const result = mustache.render(source, {
+    region: awsRegion,
+    projectName
+  });
+
+  await fs.writeFileSync(path.join(__dirname, 'eksctl.yaml'), result);
+};
+
+main()
+  .then(() => console.log('Config created.'))
+  .catch((e) => console.error(e));
