534fd314c9bb0249a919d94d203df43776ed960e
Chase Adams <c@cadams.io>
2022-06-21 20:37:28 -0700

    fix: fix for running on host

--- docker/single-orchestrator-subgraph/main.js
+++ docker/single-orchestrator-subgraph/main.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
 const port = 7402;
 
 const server = http.createServer((req, res) => {
@@ -20,6 +19,6 @@ const server = http.createServer((req, res) => {
   );
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at port ${port}/`);
 });
